43cf9db06f057ee34bf7ecf7d596d9f35967a6e0
annihilatrix <katie@katie.codes>
2017-01-15 16:55:04 -0600

    Added function to create, read, update or delete config file

--- create_config.py
+++ create_config.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 
 def create_config(file_path):
@@ -12,6 +13,34 @@ def create_config(file_path):
     with open(file_path, "w") as config_file:
         config.write(config_file)
 
+
+def crud_config(file_path):
+    """
+    Create, read, update or delete a config file.
+    :param file_path:
+    :return:
+    """
+    if not os.path.exists(file_path):
+        create_config(file_path)
+
+    config = configparser.ConfigParser()
+    config.read(file_path)
+
+    # read some values from the config
+    font = config.get("Settings", "font")
+    font_size = config.get("Settings", "font_size")
+
+    # change a vlue in the config
+    config.set("Settings", "font_size", "12")
+
+    # delete a value from the config
+    config.remove_option("Settings", "font_style")
+
+    # write changes back to config
+    with open(file_path, "w") as config_file:
+        config.write(config_file)
+
 if __name__ == "__main__":
     path = "settings.ini"
-    create_config(path)
+    # create_config(path)
+    crud_config(path)

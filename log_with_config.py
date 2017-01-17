1c8545a98621cb2d0a3fd3b171d205e1b9c79696
annihilatrix <katie@katie.codes>
2017-01-16 20:12:37 -0600

    Added logging from dict

--- log_with_config.py
+++ log_with_config.py
@@ -8,7 +8,28 @@ def main():
     Based on http://docs.python.org/howto/logging.html#configuring-logging
     :return:
     """
-    logging.config.fileConfig("config.ini")
+    dictLogConfig = {
+        "version": 1,
+        "handlers": {
+            "fileHandler": {
+                "class": "logging.FileHandler",
+                "formatter": "myFormatter",
+                "filename": "config.log"
+            }
+        },
+        "loggers": {
+            "exampleApp": {
+                "handlers": ["fileHandler"],
+                "level": "INFO",
+            }
+        },
+        "formatters": {
+            "myFormatter": {
+                "format":"%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            }
+        }
+    }
+    logging.config.dictConfig(dictLogConfig)
     logger = logging.getLogger("exampleApp")
 
     logger.info("Program started")

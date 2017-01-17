6549af90008ecb71877c43edfe047c00558d2844
annihilatrix <katie@katie.codes>
2017-01-16 18:18:22 -0600

    Included logging of another module

--- logger.py
+++ logger.py
@@ -1,15 +1,29 @@
 import logging
+import math
 
-# add filemode="w" to overwrite
-logging.basicConfig(filename="sample.log",
-                    level=logging.INFO)
-log = logging.getLogger("ex")
 
-try:
-    raise RuntimeError
-except RuntimeError:
-    log.exception("Error!")
+def main():
+    """
+    The main entry point of the application.
+    :return:
+    """
+    # add filemode="w" to overwrite
+    logging.basicConfig(filename="sample.log",
+                        level=logging.INFO)
+    logging.info("Program started")
+    result = math.fsum([7, 8])
+    logging.info("Done!")
 
-    logging.debug("This is a debug message.")
-    logging.info("Informational message.")
-    logging.error("An error has occurred!")
+    # log = logging.getLogger("ex")
+    #
+    # try:
+    #     raise RuntimeError
+    # except RuntimeError:
+    #     log.exception("Error!")
+    #
+    #     logging.debug("This is a debug message.")
+    #     logging.info("Informational message.")
+    #     logging.error("An error has occurred!")
+
+if __name__=="__main__":
+    main()
